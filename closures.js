7c1fc2161d52ef97f5452326a1c950b5456c87c4
Leo Volwahsen <volwahsenl@gmail.com>
2022-09-07 13:08:24 +0200

    g

--- closures.js
+++ closures.js
@@ -2,12 +2,11 @@ console.log("Aufgabe 1");
 
 function specialMultiply(num1, num2) {
     if (arguments.length === 2) return num1 * num2;
-    else if (arguments.length === 1) {
-        return function specialMultiply(num2) {
-            return num1 * num2;
-        };
-    }
+    return function specialMultiply(num2) {
+        return num1 * num2;
+    };
 }
+
 console.log(specialMultiply(3, 4));
 console.log(specialMultiply(3)(4));
 console.log("---------------");
